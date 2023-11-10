bbf4771eedb67d284d8f34166567495ef8c9b98d
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-10 11:21:36 +0100

    feat: snack 1

---
+++ snack-2/script.js
@@ -0,0 +1,19 @@
+
+//Faccio scrivere le parole dall'utente
+
+var parola1 = prompt("Inserisci una parola che ti piace:");
+var parola2 = prompt("Inserisci un'altra parola che ti piace:");
+
+
+//Determino la parola più corta o più lunga e poi la stampo in console
+if (parola1.length < parola2.length) {
+  console.log("La parola più corta è: " + parola1);
+  console.log("La parola più lunga è: " + parola2);
+} else if (parola2.length < parola1.length) {
+  console.log("La parola più corta è: " + parola2);
+  console.log("La parola più lunga è: " + parola1);
+
+  //Stampo un messaggio alternativo se la lunghezza è la stessa
+} else {
+  console.log("Le due parole hanno la stessa lunghezza.");
+}
