d570d0be62e0605fc4d53fc8ee86941f42d208b1
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-14 09:51:27 +0100

    feat: snack 4

---
+++ snack-4/script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+let numeriArray = [];
+
+// Chiedo all'utente di inserire numeri fino a quando la somma è inferiore a 50
+while (calcolaSomma(numeriArray) < 50) {
+
+  let numero = parseInt(prompt("Inserisci un numero:"));
+
+  // Aggiungo il numero all'array
+  numeriArray.push(numero);
+  
+  // Verifico se la somma è superiore a 50
+  if (calcolaSomma(numeriArray) >= 50) {
+    alert("La somma dei numeri ha superato i 50!");
+  }
+}
+
+// Console log
+console.log("Array finale:", numeriArray);
+
+// Funzione per calcolare la somma
+function calcolaSomma(array) {
+  return array.reduce((acc, num) => acc + num, 0);
+}
